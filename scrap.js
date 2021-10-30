3b6c3c722875d3fcbbecf7e03d6e58b5e6e93d6f
bikashranjanbhol <bikashbholranjan@gmail.com>
2021-10-30 21:08:06 +0530

    1 file modified

--- scrap.js
+++ scrap.js
@@ -1,17 +1,7 @@
-var Xray = require('x-ray');
-var x = Xray();
+async function fetchData(url) {
+  return fetch(url);
+}
 
-x('https://dribbble.com', 'li.group', [
-  {
-    title: '.dribbble-img strong',
-    image: '.dribbble-img [data-src]@data-src',
-  },
-])
-  .paginate('.next_page@href')
-  .limit(3)
-  .then(function (res) {
-    console.log(res[0]); // prints first result
-  })
-  .catch(function (err) {
-    console.log(err); // handle error in promise
-  });
+const data = await fetchData('https://amazon.in');
+
+console.log('Data', data);
