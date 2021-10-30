e15370d9d474d829970afc9bf46d5f2a71f48865
bikashranjanbhol <bikashbholranjan@gmail.com>
2021-10-30 21:03:33 +0530

    1 file modified

--- scrap.js
+++ scrap.js
@@ -1,9 +1,17 @@
 var Xray = require('x-ray');
 var x = Xray();
 
-x(
-  'https://amazon.in',
-  'title'
-)(function (err, title) {
-  console.log(title); // Google
-});
+x('https://dribbble.com', 'li.group', [
+  {
+    title: '.dribbble-img strong',
+    image: '.dribbble-img [data-src]@data-src',
+  },
+])
+  .paginate('.next_page@href')
+  .limit(3)
+  .then(function (res) {
+    console.log(res[0]); // prints first result
+  })
+  .catch(function (err) {
+    console.log(err); // handle error in promise
+  });
