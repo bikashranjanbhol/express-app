73f99c5d3bda438fd33ed0ed2fee5e7240b396d6
bikashranjanbhol <bikashbholranjan@gmail.com>
2021-10-30 21:12:14 +0530

    1 file modified

--- scrap.js
+++ scrap.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 // Make a request for a user with a given ID
 axios
-  .get('https:amazon.in')
+  .get('https://amazon.in')
   .then(function (response) {
     // handle success
     console.log(response);
