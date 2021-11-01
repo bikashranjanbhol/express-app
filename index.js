7dcf21637513e42af99cce1c0031d1d8895699b7
bikashranjanbhol <bikashbholranjan@gmail.com>
2021-11-01 08:28:34 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+// const express = require('express');
+// const app = express();
+// const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send({
-    status: 'OK',
-  });
-});
+// app.get('/', (req, res) => {
+//   res.send({
+//     status: 'OK',
+//   });
+// });
 
-app.get('/test', (req, res) => {
-  res.send({
-    router: 'test',
-    status: 'OK',
-  });
-});
+// app.get('/test', (req, res) => {
+//   res.send({
+//     router: 'test',
+//     status: 'OK',
+//   });
+// });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
+
+var http = require('http');
+
+http
+  .createServer(function (req, res) {
+    res.write('Hello, Node.js!'); //write a response to the client
+    res.end(); //end the response
+  })
+  .listen(8080); //the server object listens on port 8080
+
+console.log('Server running on port 8080');
