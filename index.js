ae468f08e1ab63fcec83f166609f2e18f5dc76de
bikashranjanbhol <bikashbholranjan@gmail.com>
2021-11-01 08:54:25 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-// const express = require('express');
-// const app = express();
-// const port = 3000;
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// app.get('/', (req, res) => {
-//   res.send({
-//     status: 'OK',
-//   });
-// });
+app.get('/', (req, res) => {
+  res.send({
+    status: 'OK',
+  });
+});
 
-// app.get('/test', (req, res) => {
-//   res.send({
-//     router: 'test',
-//     status: 'OK',
-//   });
-// });
+app.get('/test', (req, res) => {
+  res.send({
+    router: 'test',
+    status: 'OK',
+  });
+});
 
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
-var http = require('http');
+// var http = require('http');
 
-console.log(Object.values(require('os').networkInterfaces()));
+// console.log(Object.values(require('os').networkInterfaces()));
 
-http
-  .createServer(function (req, res) {
-    res.write('Hello, Node.js!'); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+// http
+//   .createServer(function (req, res) {
+//     res.write('Hello, Node.js!'); //write a response to the client
+//     res.end(); //end the response
+//   })
+//   .listen(8080); //the server object listens on port 8080
 
-console.log('Server running on port 8080');
+// console.log('Server running on port 8080');
